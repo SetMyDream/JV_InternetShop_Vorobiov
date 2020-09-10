6a682f21ac4f43d1144c881d7c0ce1d60bbc7239
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-09-10 13:09:28 +0300

    Some minor changes?

--- src/main/java/com/internet/shop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/internet/shop/service/impl/OrderServiceImpl.java
@@ -27,7 +27,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<Order> getUserOrders(Long userId) {
-        return orderDao.getAll();
+        return orderDao.getUserOrders(userId);
     }
 
     @Override
