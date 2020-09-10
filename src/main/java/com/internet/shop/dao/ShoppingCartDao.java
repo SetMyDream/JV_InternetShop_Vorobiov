b2d6df35dbce4e745cf0343b0cacde77e5d55b53
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-09-10 09:14:35 +0300

    Some changes in ShoppingCartDao

--- src/main/java/com/internet/shop/dao/ShoppingCartDao.java
+++ src/main/java/com/internet/shop/dao/ShoppingCartDao.java
@@ -1,17 +1,6 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.ShoppingCart;
-import java.util.List;
-import java.util.Optional;
 
 public interface ShoppingCartDao extends GeneralDao<ShoppingCart, Long> {
-    ShoppingCart create(ShoppingCart shoppingCart);
-
-    Optional<ShoppingCart> getById(Long shoppingCartId);
-
-    ShoppingCart update(ShoppingCart shoppingCart);
-
-    boolean delete(Long shoppingCartId);
-
-    List<ShoppingCart> getAll();
 }
